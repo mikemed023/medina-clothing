d761b6f61924048b59d7a0a8d127c1c58fd5b0f6
Michael <mike_med023@yahoo.com>
2020-02-28 21:16:22 -0500

    change google sign in into sagas

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import { googleSignInStart } from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -30,6 +32,7 @@ class SignIn extends React.Component {
 	};
 
 	render() {
+		const { googleSignInStart } = this.props;
 		return (
 			<div className="sign-in">
 				<h2>I already have an account</h2>
@@ -54,7 +57,7 @@ class SignIn extends React.Component {
 					/>
 					<div className="buttons">
 						<CustomButton type="submit"> Sign in </CustomButton>
-						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+						<CustomButton type="button" onClick={googleSignInStart} isGoogleSignIn>
 							{' '}
 							Sign in with Google{' '}
 						</CustomButton>
@@ -65,4 +68,8 @@ class SignIn extends React.Component {
 	}
 }
 
-export default SignIn;
+const mapDispatchToProps = (dispatch) => ({
+	googleSignInStart: () => dispatch(googleSignInStart())
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
